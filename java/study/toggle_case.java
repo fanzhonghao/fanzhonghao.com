73ea723b347d75b217f29f9acabf1d3534f729f7
fanzhonghao <1412328318@qq.com>
2017-06-18 10:35:07 +0800

    java/study/toggle_case.java

---
+++ java/study/toggle_case.java
@@ -0,0 +1,8 @@
+public class toggle_case{
+  public static void main(String[] args) {
+    String s = "Hello KITTY";
+    String s1 = "Good morning";
+    System.out.println("大写字母变为小写字母：" + s.toLowerCase());
+    System.out.println("小写字母变为大写字母：" + s1.toUpperCase());
+  }
+}
